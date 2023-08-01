4eb79b31e946f7dfbd5b63bfef23af6f65d75dd9
Michael Brantley <brantleyrecs@gmail.com>
2023-08-01 18:49:30 -0500

    added promises for assignments and students. also updated t he login page

--- components/Signin.js
+++ components/Signin.js
@@ -8,11 +8,15 @@ function Signin() {
       style={{
         height: '90vh',
         padding: '30px',
-        maxWidth: '400px',
+        maxWidth: '700px',
         margin: '0 auto',
       }}
     >
-      <h1>Hi there!</h1>
+      <h1>Welcome To</h1>
+      <br />
+      <h1>The Derek Zoolander Center For Children Who Can't Read... Good</h1>
+      <h4>(And Who Want To Do Other Stuff Good Too)</h4>
+      <br />
       <p>Click the button below to login!</p>
       <button type="button" className="btn btn-primary btn-lg copy-btn" onClick={signIn}>
         Sign In
--- utils/data/studentData.js
+++ utils/data/studentData.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import { clientCredentials } from '../client';
 
-export default function AssignmentForm() {
-  return (
-    <div>
-      Education
-    </div>
-  );
-}
+const endpoint = clientCredentials.databaseURL;
+
+const getStudents = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/students.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+const getSingleStudents = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/students/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const createStudent = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/students.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateStudents = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/students/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getStudents,
+  getSingleStudents,
+  createStudent,
+  updateStudents,
+};
