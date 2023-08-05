63a882e0d5df0fd01614da426135741dafbe1e0d
Michael Brantley <brantleyrecs@gmail.com>
2023-08-05 10:35:09 -0500

    work on assignments

--- pages/assignment/[firebaseKey].js
+++ pages/assignment/[firebaseKey].js
@@ -3,22 +3,26 @@ import { useRouter } from 'next/router';
 import { getSingleAssignment } from '../../utils/data/assignmentData';
 
 export default function ViewAssignment() {
-  const [assignmentDetails, setAssignmentDetails] = useState([]);
+  const [assignmentDetails, setAssignmentDetails] = useState();
   const router = useRouter();
 
   const { firebaseKey } = router.query;
 
   useEffect(() => {
     getSingleAssignment(firebaseKey).then(setAssignmentDetails);
+    console.warn(assignmentDetails);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [firebaseKey]);
 
   return (
     <>
       <div className="mt-5 d-flex flex-wrap">
+        <div className="d-flex flex-column">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={assignmentDetails.image_url} alt={assignmentDetails.title} style={{ width: '300px' }} />
+        </div>
         <div className="text-white ms-5 details">
-          <h5>
-            {assignmentDetails.title}
-          </h5>
+          <h5>{assignmentDetails.content}</h5>
           <p>{assignmentDetails.content}</p>
         </div>
       </div>
